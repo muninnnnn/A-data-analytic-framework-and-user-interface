a96414a9c38e97334846642d6e53740993fb6c23
muninnnnn <munin_hui@163.com>
2018-07-19 15:26:00 +0100

    Optimse the vector thing

--- vtuRead/main.cpp
+++ vtuRead/main.cpp
@@ -62,45 +62,7 @@ void readPvd(string file)
     //cout<<fileCounter<<endl;
 }
 
-void saveFilename(string file)
-{
-    string vtuFile[fileCounter];
-    //vector<string> vtuFile(fileCounter);
 
-    int n = 0;
-    string line;
-    string filename;
-	ifstream infile;
-
-	infile.open(file.data());
-
-	if (!infile.is_open())
-	{
-		cout << "Error,no such file\n";
-	}
-
-
-    string s1 = "file=\"";
-    while(getline(infile,line))
-    {
-        if (line.find(s1) != string::npos)
-        {
-            int location = line.find(s1);
-            location = location + 6;
-            vtuFile[n] = line.substr(location,15);
-            //vtuFile.push_back(line.substr(location,15));
-            n++;
-
-        }
-
-    }
-    for(int i = 0; i <fileCounter; i++)
-    {
-
-        cout<<vtuFile[i]<<endl;
-    }
-    infile.close();
-}
 
 
 void readCdnt(string file)
@@ -113,14 +75,7 @@ void readCdnt(string file)
 
 	stringstream numTrans;
 	int numOfPoints;
-	int nn = 0;
-
-    float x_max = 0.0;
-    float x_min = 0.0;
-    float y_max = 0.0;
-    float y_min = 0.0;
-
-
+	int num_flag = 0;
 
 
 	infile.open(file.data());
@@ -151,8 +106,8 @@ void readCdnt(string file)
             numTrans << line.substr(location1,location);
             numTrans >> numOfPoints;
 
-            cout<<"location1 is: "<<location1<<endl;
-            cout<<"location2 is: "<<location2<<endl;
+            //cout<<"location1 is: "<<location1<<endl;
+            //cout<<"location2 is: "<<location2<<endl;
             cout<<"Number of Points is: "<<numOfPoints<<endl;
 
 
@@ -166,42 +121,27 @@ void readCdnt(string file)
 
     cout<<endl;
 
-    float cdnt[numOfPoints][2];
+    //float cdnt[numOfPoints][2];
 
-    //vector<vector<float> >cdnt(fileCounter,vector<float>(2,0));
+    vector<vector<float> >cdnt;
+
+    for(int i = 0; i < numOfPoints; i++)
+    {
+        cdnt.push_back(vector<float>(2));
+    }
 
     while (getline(infile,line))
     {
-        //cout<<line<<endl;
-
-		float first_num, second_num, third_num;
+      	float first_num, second_num, third_num;
 		sscanf(line.c_str(), "%f %f %f", &first_num, &second_num, &third_num);
 		//cout << first_num << " " << second_num << " " << third_num << endl;
-		cdnt[nn][0] = first_num;
-		cdnt[nn][1] = second_num;
+		cdnt[num_flag][0] = first_num;
+		cdnt[num_flag][1] = second_num;
 		vec_num.push_back(first_num);
-		nn++;
-/*
-		if(cdnt[nn][0] > x_max)
-            {
-                x_max = cdnt[nn][0];
-            }
-        if(cdnt[nn][0] < x_min)
-            {
-                x_min = cdnt[nn][0];
-            }
-        if(cdnt[nn][1] > y_max)
-            {
-                y_max = cdnt[nn][1];
-            }
-        if(cdnt[nn][0] < y_min)
-            {
-                y_min = cdnt[nn][1];
-            }
+		num_flag++;
 
-*/
 
-		if(nn >= numOfPoints)
+		if(num_flag >= numOfPoints)
             break;
 	}
 
@@ -210,15 +150,9 @@ void readCdnt(string file)
 	for (int i = 0; i < numOfPoints; i++)
 	{
 	    outfile << cdnt[i][0] << " " << cdnt[i][1] << endl;
-		//cout << cdnt[i][0] << " " << cdnt[i][1] << endl;
+		cout << cdnt[i][0] << " " << cdnt[i][1] << endl;
 	}
 	outfile << endl;
-	cout<<endl;
-
-	cout<<"The max x is :"<<x_max<<endl;
-    cout<<"The min x is :"<<x_min<<endl;
-    cout<<"The max y is :"<<y_max<<endl;
-    cout<<"The min y is :"<<x_min<<endl;
 
 	infile.close();
 
@@ -231,20 +165,10 @@ void readCdnt(string file)
 int main()
 {
 
-	//readTxt("C:\\Users\\munin\\Desktop\\Project\\example_meshpoints_10\\example_meshpoints_10\\worm.pvd");
-    //readTxt("/home/csunix/sc17dh/Project/sampeVTUs/worm_000010.vtu");
-    //readTxt("/home/csunix/sc17dh/Project/example_meshpoints_10/worm_000000.vtu");
-	//readCdnt("/home/csunix/sc17dh/Project/example_meshpoints_10/worm_000000.vtu");
-	//readCdnt("/home/csunix/sc17dh/Project/sampeVTUs/worm_000010.vtu");
-
-
-    readPvd("/home/csunix/sc17dh/Project/example_meshpoints_10/worm.pvd");
-    //saveFilename("/home/csunix/sc17dh/Project/example_meshpoints_10/worm.pvd");
-
-
     string file="/home/csunix/sc17dh/Project/example_meshpoints_10/worm.pvd";
-    string vtuFile[fileCounter];
-    //vector<string> vtuFile(fileCounter);
+    readPvd(file);
+
+    vector<string> vtuFile(fileCounter);
 
     string line;
     string filename;
@@ -296,6 +220,5 @@ int main()
 
 	return 0;
 }
-
 
 
