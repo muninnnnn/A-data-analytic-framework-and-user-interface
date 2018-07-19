e5ec6062e63b2fb2a0b13d50ac2a272986b66824
muninnnnn <munin_hui@163.com>
2018-07-19 18:07:18 +0100

    Finish solution2

--- vtuRead/main.cpp
+++ vtuRead/main.cpp
@@ -66,10 +66,81 @@ void readPvd(string file)
 
 
 
+void compare_size(string file)
+{
+
+    string line;
+	ifstream infile;
+
+	ofstream outfile;
+
+	stringstream numTrans;
+	int numOfPoints;
+	int num_flag = 0;
+
+
+	infile.open(file.data());
+	if (!infile.is_open())
+	{
+		cout << "Error,no such file\n";
+	}
+
+
+    string s1 = "Piece NumberOfPoints";
+    string s2 = "<DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"ascii\">";
+    string s3 = "\"";
+
+    while(getline(infile,line))
+    {
+
+
+	    if(line.find(s1) != string::npos)
+        {
+
+           int location1 = line.find(s1) + 22;
+           int location2 = line.find(s3,location1);
+           int location = location2 - location1;
+
+            numTrans << line.substr(location1,location);
+            numTrans >> numOfPoints;
+
+        }
+
+
+        if (line.find(s2) != string::npos)
+            break;
+
+    }
+
+    while (getline(infile,line))
+    {
+      	float first_num, second_num, third_num;
+		sscanf(line.c_str(), "%f %f %f", &first_num, &second_num, &third_num);
+
+		if (num_flag == 0)
+		{
+		    x_max = x_min =first_num;
+		    y_max = y_min =second_num;
+
+		}
+
+		x_max = max(first_num, x_max);
+		x_min = min(first_num, x_min);
+		y_max = max(second_num, y_max);
+		y_min = min(second_num, y_min);
+		vec_num.push_back(first_num);
+		num_flag++;
+
+		if(num_flag >= numOfPoints)
+            break;
+
+	}
+}
+
 
 void readCdnt(string file)
 {
-    //cout<<"The file name is: "<<file<<endl;
+    cout<<"The file name is: "<<file<<endl;
     string line;
 	ifstream infile;
 
@@ -110,7 +181,7 @@ void readCdnt(string file)
 
             //cout<<"location1 is: "<<location1<<endl;
             //cout<<"location2 is: "<<location2<<endl;
-            //cout<<"Number of Points is: "<<numOfPoints<<endl;
+            cout<<"Number of Points is: "<<numOfPoints<<endl;
 
 
         }
@@ -139,28 +210,14 @@ void readCdnt(string file)
 		//cout << first_num << " " << second_num << " " << third_num << endl;
 		cdnt[num_flag][0] = first_num;
 		cdnt[num_flag][1] = second_num;
-
-		if (num_flag == 0)
-		{
-		    x_max = x_min =first_num;
-		    y_max = y_min =second_num;
-
-		}
-
-		x_max = max(first_num, x_max);
-		x_min = min(first_num, x_min);
-		y_max = max(second_num, y_max);
-		y_min = min(second_num, y_min);
-
 		vec_num.push_back(first_num);
 		num_flag++;
-
 
 		if(num_flag >= numOfPoints)
             break;
 
-
 	}
+
 
     outfile << "The file name is: "<<file<<endl<<"Number of Points is: "<<numOfPoints<<endl;
 
@@ -170,6 +227,45 @@ void readCdnt(string file)
 		//cout << cdnt[i][0] << " " << cdnt[i][1] << endl;
 	}
 	outfile << endl;
+
+	 float x_width = x_max - x_min;
+     float y_width = y_max - y_min;
+
+     float size_wormworld = max(x_width, y_width);
+
+     //cout<<"The width of x is "<<x_width<<endl;
+     //cout<<"The width of y is "<<y_width<<endl;
+
+     //cout<<"The size of wormworld is "<<size_wormworld<<endl;
+
+     float x_centre = x_min + size_wormworld/2;
+     float y_centre = y_min + size_wormworld/2;
+
+     //cout<<"The centre of x is "<<x_centre<<endl;
+     //cout<<"THe centre of y is "<<y_centre<<endl;
+
+    float size_scale = 480/size_wormworld;
+
+    int x_move = 250-(size_scale * x_centre);
+    int y_move = 250-(size_scale * y_centre);
+
+    //cout<<"The move in x is :"<<x_move<<endl;
+    //cout<<"The move in y is :"<<y_move<<endl;
+
+    vector<int>pixel_coordinate_x(numOfPoints);
+    vector<int>pixel_coordinate_y(numOfPoints);
+
+    for(int i = 0; i < numOfPoints; i++)
+    {
+        pixel_coordinate_x[i] = cdnt[i][0] * size_scale + x_move;
+        pixel_coordinate_y[i] = cdnt[i][0] * size_scale + y_move;
+
+        cout<<"The x is:"<<pixel_coordinate_x[i]<<endl;
+        cout<<"The y is:"<<pixel_coordinate_y[i]<<endl;
+        cout<<endl;
+    }
+
+
 
 	infile.close();
 
@@ -222,6 +318,13 @@ int main()
 
     cout<<"The number of files is :"<<n<<endl;
 
+
+    for (int i = 0; i < n; i++)
+    {
+        //cout<<vtuFile[i]<<endl;
+        compare_size(vtuFile[i]);
+    }
+
     for (int i = 0; i < n; i++)
     {
         //cout<<vtuFile[i]<<endl;
@@ -229,10 +332,8 @@ int main()
     }
 
     //cout<<"File num is:"<<fileCounter<<endl;
-     cout<<"The x max is "<<x_max<<endl;
-     cout<<"The x min is "<<x_min<<endl;
-     cout<<"The y max is "<<y_max<<endl;
-     cout<<"The y min is "<<y_min<<endl;
+
+
 
 
 
